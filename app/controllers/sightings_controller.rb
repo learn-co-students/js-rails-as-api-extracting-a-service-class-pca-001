c6a2bd637e522bd940401b1b2ce09612551fabfc
Madison McCardle <madison.mccardle@powerhrg.com>
2020-07-14 16:33:40 -0400

    Done.

--- app/controllers/sightings_controller.rb
+++ app/controllers/sightings_controller.rb
@@ -1,6 +1,16 @@
 class SightingsController < ApplicationController
-    def show
-        @sighting = Sighting.find(params[:id])
-        render json: @sighting.to_json(:include => {:bird => {:only =>[:name, :species]}, :location => {:only =>[:latitude, :longitude]}}, :except => [:updated_at])
-    end
+  def index
+    sightings = Sighting.all
+#     render json: sightings.to_json(:include => {
+#       :bird => {:only => [:name, :species]},
+#       :location => {:only => [:latitude, :longitude]}
+#   }, :except => [:updated_at])
+# replace the original render statements with our new service class:
+    render json: SightingSerializer.new(sightings).to_serialized_json
+  end
+
+  def show
+    @sighting = Sighting.find(params[:id])
+    render json: SightingSerializer.new(sightings).to_serialized_json
+  end
 end
